0b5a70f8157b09916ac89a66965b0be1b06cffbc
Pradipta27 <52622790+Pradipta27@users.noreply.github.com>
2019-12-17 23:00:22 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect,url_for,flash
+from mysql import connector
 
 app = Flask(__name__)
-
+db = connector.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "",
+    database= "rebahan-homestay"
+)
+if db.is_connected():
+    print('berhasil terhubung ke database')
+#---------------------------------------------------------------------------------------------#
 @app.route('/')
-def template_engine():
+def halaman_utama():
     return render_template('index.html')
 
+@app.route('/register')
+def guestRegister():
+    return render_template('tambah.html')
+
+@app.route('/guest_tambah/', methods=['POST'])
+def guest_register():
+    guestId = request.form['guestId']
+    guestPw = request.form['guestPw']
+    guestName = request.form['guestName']
+    guestContact = request.form['guestContact']
+    cur = db.cursor()
+    cur.execute('insert into guest (guestId, guestPw, guestName, guestContact) values (%s, %s, %s, %s)', (guestId, guestPw, guestName, guestContact))
+    db.commit()
+    return redirect(url_for('halaman_utama'))
 if __name__ == '__main__':
     app.run()
+
+@app.route('/guesttampil')
+def guestTampil():
+    cur = db.cursor()
+    cur.execute("Select * from guest")
+    res = cur.fetchall()
+    cur.close()
+    return render_template('GuestTampil.html',hasil=res)
